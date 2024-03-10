1c698e89c5cb3d59a59b2b1ef2f8ba362e3a1bf9
afkzoro <samueltheophilus019@gmail.com>
2024-03-10 16:28:24 +0100

    update: complete signup feature with argon

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,52 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class AuthService {
-    signup() {
-        return { msg: 'This is sign up'}
+    constructor(private prisma: PrismaService) {}
+
+    async signup(dto: AuthDto) {
+        try {
+            const hash = await argon.hash(dto.password);
+
+            // Save the new user in the database
+            const user = await this.prisma.user.create({
+                data: {
+                    email: dto.email,
+                    hash,
+                },
+
+            })
+
+            delete user.hash
+            // Return the saved user
+            return user
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken');
+                }
+            }
+            
+        }
+        // generate the password hash
+        const hash = await argon.hash(dto.password);
+
+        // Save the new user in the database
+        const user = await this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash,
+            },
+
+        })
+
+        delete user.hash
+        // Return the saved user
+        return user
     }
 
     signin() {
