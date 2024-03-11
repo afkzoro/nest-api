67878b314f7656a0b4810cf47b457c53311aedfb
afkzoro <samueltheophilus019@gmail.com>
2024-03-11 08:09:34 +0100

    chore: include JWTservice in contructor

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,12 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
+    constructor(private prisma: PrismaService,
+        private jwt: JwtService) {}
 
     async signup(dto: AuthDto) {
         try {
@@ -70,8 +72,5 @@ export class AuthService {
         // Send back to user
         delete user.hash
         return user;
-
-        // send back to the user
-        return { msg: 'This is a sign in' }
     }
 }
