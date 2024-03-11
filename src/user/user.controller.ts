f994f012405b3a391a17104a83b7b8218c0cab6e
afkzoro <samueltheophilus019@gmail.com>
2024-03-11 22:15:57 +0100

    feat: import JwtGuard to clean code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, UseGuards, Req} from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { JwtGuard } from 'src/auth/guard';
 
 
 @Controller('users')
 export class UserController {
     
     // route => /users/me; Protect route with JWT token 
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(JwtGuard)
     @Get('me')
     getme(@Req() req: Request) {
         return req.user
